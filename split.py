de1f75f0859767b95dba9882f2b417b02c578f3f
Raghavendra-Sunkollu <sunkolluraghavendra@gmail.com>
2020-10-18 12:54:29 +0530

    new program done

---
+++ split.py
@@ -0,0 +1,7 @@
+txt = "welcome to the jungle the jungle is a place where There was a time when storybooks were the only means of changed now – in the age of the internetbe."
+
+x = txt.split()
+k=x.count('the')
+print(x)
+print(k)
+
